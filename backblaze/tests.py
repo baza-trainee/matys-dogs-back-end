6bdd26543d54d6daf70dd4752ab9bbd90749997a
TIU <jabsoluty@gmail.com>
2023-12-23 18:04:45 +0100

    add two routes for upload

--- backblaze/tests.py
+++ backblaze/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.core.files.uploadedfile import SimpleUploadedFile
+from backblaze.views import upload_image, upload_document
 
-# Create your tests here.
+class TestViews(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_upload_image(self):
+        image = SimpleUploadedFile("test_image.jpg", b"file_content", content_type="image/jpeg")
+        response = self.client.post('/upload/images', {'image': image})
+        self.assertEqual(response.status_code, 201)
+
+    def test_upload_document(self):
+        document = SimpleUploadedFile("test_document.pdf", b"file_content", content_type="application/pdf")
+        response = self.client.post('/upload/documents', {'document': document})
+        self.assertEqual(response.status_code, 201)
--- backblaze/urls.py
+++ backblaze/urls.py
@@ -1,7 +1,7 @@
 from . import views
 from django.urls import path, include
-from backblaze.views import upload_image
+from backblaze.views import upload_image, upload_document
 urlpatterns = [
-    path('test', views.test, name='test'),
-    path('images', upload_image, name='images')
+    path('images', upload_image, name='upload_image'),
+    path('documents', upload_document, name='upload_document')
 ]
