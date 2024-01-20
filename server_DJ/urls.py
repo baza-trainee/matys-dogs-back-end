27d948aef72d2810815b054c6425fdcf5de73775
TIU <jabsoluty@gmail.com>
2024-01-20 17:14:07 +0100

    fix: issue-11

--- server_DJ/urls.py
+++ server_DJ/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic import RedirectView
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,3 +31,6 @@ urlpatterns = [
     path('api/docs/swagger-ui', SpectacularSwaggerView.as_view(url_name='schema'),
          name='swagger-ui')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += [re_path(r'^favicon\.ico$', RedirectView.as_view(
+    url='/static/images/favicon.ico', permanent=True)),]
